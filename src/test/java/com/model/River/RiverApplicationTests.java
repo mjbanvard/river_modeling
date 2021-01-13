5da10f6c2e4de9afb82c4b159842bffd120b31f0
Michael Banvard <mjbanvard@gmail.com>
2021-01-13 15:16:11 -0600

    first commit

---
+++ src/test/java/com/model/River/RiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model.River;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
